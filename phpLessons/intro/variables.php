eb4d4d2067eeb4e0d1b9d0c4fb963631686fb165
rokpopov <rokpopov90@gmail.com>
2021-05-15 16:35:39 +0200

    declaring variables

---
+++ phpLessons/intro/variables.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+  <?php
+    $var1 = "Declaring variables";
+    $x = 15;
+    $y = 20.5;
+
+    echo $var1;
+    echo "<br>";
+    echo $x;
+    echo "<br>";
+    echo $y;
+    echo "<hr>";
+
+    $sport = "formula 1";
+    echo "When is the next $sport race?";
+    echo "<hr>";
+
+    echo "Who spun?";
+    echo "<br >";
+    $Nikita = "MazeSpin";
+    echo "Nikita " . $Nikita . ", who else!";
+    echo "<hr>";
+
+    $x = 5;
+    $y = 45;
+    echo $x + $y;
+  ?>
+
+</body>
+
+</html>
