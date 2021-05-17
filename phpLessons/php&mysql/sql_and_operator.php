e75edd87255d9b924ac5255b76ffed5d6d19a427
rokpopov <rokpopov90@gmail.com>
2021-05-17 23:48:39 +0200

    fetch the email of  the user with a specific fistname and lastname value

---
+++ phpLessons/php&mysql/sql_and_operator.php
@@ -0,0 +1,35 @@
+<?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "anticefur3d";
+  $dbname = "mywebsite";
+
+  // create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+
+  //check connection
+  if($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $sql = "SELECT email FROM users
+  WHERE firstname='Simone'
+  AND lastname='Limoni'
+  ";
+  $result = $conn->query($sql);
+
+  if($result->num_rows > 0) {
+
+
+    while($row = $result->fetch_assoc()) {
+      // output data of each row
+      echo "Email: " . $row["email"]. "<br>";
+    }
+      } else {
+      echo "0 results";
+    }
+
+
+  $conn->close();
+ ?>
