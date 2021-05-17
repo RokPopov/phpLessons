5c2b34a3dca7bce0c545ec799993f269f0c99cfa
rokpopov <rokpopov90@gmail.com>
2021-05-17 23:43:10 +0200

    select users where firstname is a specific value

---
+++ phpLessons/php&mysql/select_where.php
@@ -0,0 +1,32 @@
+<?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "anticefur3d";
+  $dbname = "mywebsite";
+
+  // create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+
+  //check connection
+  if($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $sql = "SELECT * FROM users WHERE firstname='Zac';";
+  $result = $conn->query($sql);
+
+  if($result->num_rows > 0) {
+
+
+    while($row = $result->fetch_assoc()) {
+      // output data of each row
+      echo "Name: " . $row["firstname"]. "<br>";
+    }
+      } else {
+      echo "0 results";
+    }
+
+
+  $conn->close();
+ ?>
